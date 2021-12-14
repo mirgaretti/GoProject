ba394fb8bb17cfbb5ea31ed43a473b4d8d0d27fd
mirgaretti <margarita.p.chernova@gmail.com>
2021-12-14 03:55:01 +0300

    tasks service api key

--- Tasks/src/handler/handler.go
+++ Tasks/src/handler/handler.go
@@ -9,7 +9,13 @@ import (
 )
 
 func ClassHandler(c echo.Context) error {
-	
+	apikey := c.Request().Header.Get("X-API-KEY")
+	if apikey != "8f9c9f1d-860e-4665-9b16-14b2b462e2b2" {
+		response := model.ResponseDto {
+			Message: "Unauthorized request",
+		}
+		return c.JSON(http.StatusOK, response)
+	}
 	classes, err := repository.GetAllClasses()
 	if err != nil{
 		response := model.ResponseDto {
